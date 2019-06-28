c9990520aa3e0243f869b14568d76f0171164e1b
Riyuu_Kim_Beijing <526719872@qq.com>
2019-06-28 11:13:43 +0800

    1

---
+++ sp10/src/main/java/com/tedu/sp10/Sp10Application.java
@@ -0,0 +1,13 @@
+package com.tedu.sp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp10Application.class, args);
+	}
+
+}
